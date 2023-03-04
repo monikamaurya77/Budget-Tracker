09575c81e764e60245851a08c6db06bf20239ad1
Monika Maurya <96534851+monikamaurya77@users.noreply.github.com>
2023-03-05 03:37:45 +0530

    Update AppContext.js

--- src/components/Context/AppContext.js
+++ src/components/Context/AppContext.js
@@ -21,7 +21,7 @@ function reducer(state,action){
         case 'DELETE_ITEM':
             let newState2 = {...state};
             newState2['expenses'] = newState2.expenses.filter((item)=>{
-                if(item.id!==action.payload)
+                if(item.id!=action.payload)
                     return item;
             });
             console.log('new expenses is',newState2);
